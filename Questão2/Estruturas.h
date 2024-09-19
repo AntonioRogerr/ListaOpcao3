520fd11fb4722864bf062e7e6016e7fac859244e
Antonio Roger <137258123+AntonioRogerr@users.noreply.github.com>
2024-09-19 15:09:37 -0300

    Update Estruturas.h

--- Questão2/Estruturas.h
+++ Questão2/Estruturas.h
@@ -1,86 +1,38 @@
 //Discente: Antonio Roger Sousa de Morais - 202311140022
 //Discente: Pedro Ferreira Brandão - 202311140001
 
-#include "Estruturas.h"
-#include <iostream>
+#ifndef ESTRUTURAS_H
+#define ESTRUTURAS_H
 
-// Construtor da Fila
-Fila::Fila() : inicio(nullptr), fim(nullptr) {}
+struct Node {
+    int valor;
+    Node* prox;
+};
 
-// Destrutor da Fila
-Fila::~Fila() {
-    while (!vazia()) {
-        desenfileirar();
-    }
-}
+class Fila {
+private:
+    Node* inicio;
+    Node* fim;
 
-// Enfileirar na Fila
-void Fila::enfileirar(int valor) {
-    Node* novo = new Node{valor, nullptr};
-    if (vazia()) {
-        inicio = fim = novo;
-    } else {
-        fim->prox = novo;
-        fim = novo;
-    }
-}
+public:
+    Fila();
+    ~Fila();
+    void enfileirar(int valor);
+    int desenfileirar();
+    bool vazia() const;
+};
 
-// Desenfileirar da Fila
-int Fila::desenfileirar() {
-    if (vazia()) {
-        return -1;
-    }
-    Node* temp = inicio;
-    int valor = temp->valor;
-    inicio = inicio->prox;
-    delete temp;
-    return valor;
-}
+class Pilha {
+private:
+    Node* topo;
 
-// Verificar se a Fila está vazia
-bool Fila::vazia() const {
-    return inicio == nullptr;
-}
+public:
+    Pilha();
+    ~Pilha();
+    void empilhar(int valor);
+    int desempilhar();
+    bool vazia() const;
+    void exibir() const;
+};
 
-// Construtor da Pilha
-Pilha::Pilha() : topo(nullptr) {}
-
-// Destrutor da Pilha
-Pilha::~Pilha() {
-    while (!vazia()) {
-        desempilhar();
-    }
-}
-
-// Empilhar na Pilha
-void Pilha::empilhar(int valor) {
-    Node* novo = new Node{valor, topo};
-    topo = novo;
-}
-
-// Desempilhar da Pilha
-int Pilha::desempilhar() {
-    if (vazia()) {
-        return -1;
-    }
-    Node* temp = topo;
-    int valor = temp->valor;
-    topo = topo->prox;
-    delete temp;
-    return valor;
-}
-
-// Verificar se a Pilha está vazia
-bool Pilha::vazia() const {
-    return topo == nullptr;
-}
-
-// Exibir os elementos da Pilha
-void Pilha::exibir() const {
-    Node* atual = topo;
-    while (atual != nullptr) {
-        std::cout << atual->valor << " ";
-        atual = atual->prox;
-    }
-    std::cout << std::endl;
-}
+#endif
